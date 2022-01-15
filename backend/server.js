73007f7cba79f7443d6f0147f60f9bedf1d121c4
Maddy246 <mayur.maluskar@gmail.com>
2022-01-15 23:35:57 +0530

    final build

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/dbConnection");
 const userRoute = require("./routes/userRoute");
+const notesRoute = require("./routes/notesRoute");
 const { notFound, handleError } = require("./Middleware/errorMiddleware");
-const notesData = require("./data/notes");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -13,33 +14,24 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 6000;
 
-// Add Access Control Allow Origin headers to avoid cors policy error
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*"); // * to allow all the sites we can give url too
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
-app.get("/", (req, res) => {
-  res.send(`api is running....`);
-});
-
-app.get("/api/notes", (req, res) => {
-  res.json(notesData);
-});
-
-app.get("/api/notes/:id", (req, res) => {
-  const noteId = req.params.id;
-  const note = notesData.find((n) => {
-    return n.id === noteId;
-  });
-  res.send(note);
-});
-
 app.use("/api/users", userRoute);
+app.use("/api/notes", notesRoute);
+
+//deployment
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
 app.use(notFound);
 app.use(handleError);
 
